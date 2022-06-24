ff4d41b29bdd1d3e7ebf1b46b7897717f62be124
vitoriajaco <vitoriajaco@gmail.com>
2022-06-24 16:09:11 -0300

    Corrigir nome de classe

--- src/MaiorNumero.java
+++ src/MaiorNumero.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class maiorNumero {
+public class MaiorNumero {
     public static void main(String[] args) {
         Scanner  input = new Scanner(System.in);
         System.out.println("Digite um numero inteiro:");
