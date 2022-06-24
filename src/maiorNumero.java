3b3ccf87de8045bc01a4e5f9de44ad50f51d1175
vitoriajaco <vitoriajaco@gmail.com>
2022-06-24 14:58:39 -0300

    Adicionando exercicio1

---
+++ src/maiorNumero.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class maiorNumero {
+    public static void main(String[] args) {
+        Scanner  input = new Scanner(System.in);
+        System.out.println("Digite um numero inteiro:");
+        int primeiroNumero = input.nextInt();
+        System.out.println("Digite um segundo numero");
+        int segundoNumero = input.nextInt();
+
+        if (primeiroNumero > segundoNumero)
+            System.out.println("O maior numero é " + primeiroNumero);
+        else
+            System.out.println("O maior numero é " + segundoNumero);
+
+    }
+}
