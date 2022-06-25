1bf735890d713392a069ecea89341c371c40f8d9
vitoriajaco <vitoriajaco@gmail.com>
2022-06-24 21:33:22 -0300

    Questão 1

---
+++ src/ListaB/RepeteAteCem.java
@@ -0,0 +1,17 @@
+package ListaB;
+
+public class RepeteAteCem {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++ ){
+            System.out.println(i);
+        }
+
+        int contador = 1;
+        while (contador <= 100) {
+            System.out.println(contador);
+            contador++;
+        }
+
+    }
+}
