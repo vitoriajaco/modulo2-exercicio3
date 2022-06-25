3cc1b4931069e22a8e4f34cfe45f42a4ab6bebec
vitoriajaco <vitoriajaco@gmail.com>
2022-06-24 21:53:01 -0300

    Questão 2

---
+++ src/ListaB/MediaNumeros.java
@@ -0,0 +1,21 @@
+package ListaB;
+
+import java.util.Scanner;
+
+public class MediaNumeros {
+    public static void main(String[] args) {
+        System.out.println("Quantos numeros você deseja inserir: ");
+        Scanner input = new Scanner(System.in);
+        int quantidade = input.nextInt();
+
+        int  soma = 0;
+        for (int i=1; i <= quantidade; i++) {
+            System.out.println("Digite o valor " + i +" :");
+            Scanner input2 = new Scanner(System.in);
+            int valor = input2.nextInt();
+            soma += valor;
+        }
+        System.out.println("A média é: " + (soma/quantidade));
+
+    }
+}
