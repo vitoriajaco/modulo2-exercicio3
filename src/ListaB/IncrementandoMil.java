3ce5b896091bca9a2b494c04db9680bc8f7d724e
vitoriajaco <vitoriajaco@gmail.com>
2022-06-28 21:41:36 -0300

    Exercicio 5

---
+++ src/ListaB/IncrementandoMil.java
@@ -0,0 +1,9 @@
+package ListaB;
+
+public class IncrementandoMil {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 100001; i+=1000)
+            System.out.println(i);
+    }
+}
