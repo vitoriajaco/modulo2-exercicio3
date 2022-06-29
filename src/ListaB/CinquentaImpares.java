0d2661d4695330abd59e87ab8b24edffd92761f8
vitoriajaco <vitoriajaco@gmail.com>
2022-06-28 21:14:47 -0300

    Exercicio 8

---
+++ src/ListaB/CinquentaImpares.java
@@ -0,0 +1,22 @@
+package ListaB;
+
+import java.util.Scanner;
+
+public class CinquentaImpares {
+    public static void main(String[] args) {
+
+
+        int valorSoma = 0;
+        for ( int i = 1; i <= 50; i++) {
+            if ( i % 2== 1); {
+                valorSoma += i;
+
+            }
+
+        }
+        System.out.println(valorSoma);
+        System.exit(1);
+
+
+    }
+}
